5c2bcaa0f71ec64dddd963129442fd3d493c21e2
tedigc <tedigc@gmail.com>
2017-01-10 20:18:42 +0000

    Excerpt are now written to db

--- server/src/controllers/excerpts.js
+++ server/src/controllers/excerpts.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var authenticate = require('../middlewares/authenticate');
 
+var Excerpt = require('../models/excerpts');
+
 var router = express.Router();
 
 router.post('/', authenticate, function(req, res) {
-  console.log(req.currentUser.attributes);
-  console.log(req.body);
-  res.status(201).json({ success : true });
+  var title = req.body.title;
+  var excerpt = req.body.excerpt;
+
+  // Write the excerpt to the database
+  Excerpt.forge({
+    title  : title,
+    excerpt: excerpt,
+    ownerId: req.currentUser.attributes.id
+  }, { hasTimestamps: true})
+    .save(null, { method: 'insert'})
+    .then(function(user) {
+      res.json({ success: true });
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+    
 });
 
 module.exports = router;
---
+++ server/src/migrations/20170110192555_excerpts.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('excerpts', function(table) {
+    table.increments();
+    table.string('title').notNullable();
+    table.string('excerpt').notNullable();
+    table.bigInteger('ownerId').unsigned().index().references('id').inTable('users');
+    table.timestamps();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('excerpt');
+};
