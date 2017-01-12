7e15b3dfe6972df574c06b44b0bacb531222d8e2
tedigc <tedigc@gmail.com>
2017-01-12 13:47:21 +0000

    Minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-// import DashboardContainer from './containers/dashboard/DashboardContainer';
-// import LandingPageContainer from './containers/landingpage/LandingPageContainer';
 import routes from './routes';
 
 class App extends React.Component {
 
   render() {
-
-    // let container = null;
-    // if(this.props.auth.isAuthenticated) {
-    //   // if authenticated, show the dashboard
-    //   container = <DashboardContainer/>;
-    // } else {
-    //   // else, show the landing page
-    //   container = <LandingPageContainer/>
-    // }
-
     return <Router history={browserHistory} routes={routes}/>;
   }
 
